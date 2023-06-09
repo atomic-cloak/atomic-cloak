0283750e11ce77b1f96576a16ac8d3ebab3a2d53
Tommy <totaylor19@gmail.com>
2023-06-09 12:34:05 +0200

    Added frontend boilerplate

---
+++ app/src/pages/_app.js
@@ -0,0 +1,17 @@
+import { ThirdwebProvider } from "@thirdweb-dev/react";
+import "../styles/globals.css";
+
+// This is the chain your dApp will work on.
+// Change this to the chain your app is built for.
+// You can also import additional chains from `@thirdweb-dev/chains` and pass them directly.
+const activeChain = "ethereum";
+
+function MyApp({ Component, pageProps }) {
+  return (
+    <ThirdwebProvider activeChain={activeChain}>
+      <Component {...pageProps} />
+    </ThirdwebProvider>
+  );
+}
+
+export default MyApp;
