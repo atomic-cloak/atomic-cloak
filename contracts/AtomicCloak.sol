bb10b1e87e7cd9de22236ca441948a8e2d176edf
Alessandro Ricottone <ricott2@gmail.com>
2023-06-09 14:01:03 +0200

    updated file

--- contracts/AtomicCloak.sol
+++ contracts/AtomicCloak.sol
@@ -13,7 +13,10 @@ contract AtomicCloak {
         uint256 value;
         address payable sender;
         address payable recipient;
-        bytes secretKey;
+        bytes32 secretKey;
+        bytes32 qx;
+        bytes32 qy;
+        Fees fee;
     }
 
     enum States {
@@ -29,46 +32,84 @@ contract AtomicCloak {
         NORMAL
     }
 
-    mapping(bytes32 => Swap) private swaps;
-    mapping(bytes32 => States) private swapStates;
+    mapping(address => Swap) private swaps;
+    mapping(address => States) private swapStates;
     address immutable ETH_TOKEN_CONTRACT = address(0x0);
+    uint256 immutable gx =
+        0x79be667ef9dcbbac55a06295ce870b07029bfcdb2dce28d959f2815b16f81798;
+    uint256 immutable gy =
+        0x483ada7726a3c4655da4fbfc0e1108a8fd17b448a68554199c47d08ffb10d4b8;
+    uint8 immutable gyParity = 27; //==gy % 2 != 0 ? 28 : 27
 
-    event Open(bytes32 _swapID, address _recipient);
-    event Expire(bytes32 _swapID);
-    event Close(bytes32 _swapID, bytes _secretKey);
+    event Open(address _swapID, address _recipient);
+    event Expire(address _swapID);
+    event Close(address _swapID, bytes32 _secretKey);
 
-    modifier onlyInvalidSwaps(bytes32 _swapID) {
+    modifier onlyInvalidSwaps(address _swapID) {
         require(swapStates[_swapID] == States.INVALID);
         _;
     }
 
-    modifier onlyOpenSwaps(bytes32 _swapID) {
+    modifier onlyOpenSwaps(address _swapID) {
         require(swapStates[_swapID] == States.OPEN);
         _;
     }
 
-    modifier onlyClosedSwaps(bytes32 _swapID) {
+    modifier onlyClosedSwaps(address _swapID) {
         require(swapStates[_swapID] == States.CLOSED);
         _;
     }
 
-    modifier onlyExpirableSwaps(bytes32 _swapID) {
+    modifier onlyExpirableSwaps(address _swapID) {
         require(block.timestamp >= swaps[_swapID].timelock);
         _;
     }
 
-    modifier onlyWithSecretKey(bytes32 _swapID, bytes memory _secretKey) {
-        // TODO: Inject Schnorr verification;
+    modifier onlyWithSecretKey(address _swapID, bytes32 _secretKey) {
+        // Inject Schnorr verification;
         // Note: _swapID is actually the commitment
-        require(_swapID == sha256(_secretKey));
+        require(ecmulVerify(_secretKey, _swapID));
+        // require(_swapID == sha256(_secretKey));
         _;
     }
 
+    function ecmulVerify(
+        bytes32 _secretKey,
+        address _swapID
+    ) public pure returns (bool) {
+        uint256 m = 0xFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFEBAAEDCE6AF48A03BBFD25E8CD0364141;
+
+        address signer = ecrecover(
+            0,
+            gyParity,
+            bytes32(gx),
+            bytes32(mulmod(uint256(_secretKey), gx, m))
+        );
+
+        return signer == _swapID;
+    }
+
+    function commitmentToAddress(
+        bytes32 _qx,
+        bytes32 _qy
+    ) public pure returns (address) {
+        address _addr = address(
+            uint160(
+                uint256(keccak256(abi.encodePacked(_qx, _qy))) &
+                    0x00FFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF
+            )
+        );
+        return _addr;
+    }
+
     function openETH(
-        bytes32 _swapID,
+        bytes32 _qx,
+        bytes32 _qy,
         address payable _recipient,
         uint256 _timelock
     ) public payable {
+        address _swapID = commitmentToAddress(_qx, _qy);
+
         // The swapID is used also as commitment
         require(
             swapStates[_swapID] == States.INVALID,
@@ -86,8 +127,12 @@ contract AtomicCloak {
             value: msg.value,
             sender: payable(msg.sender),
             recipient: _recipient,
-            secretKey: new bytes(0)
+            secretKey: bytes32(0),
+            qx: _qx,
+            qy: _qy,
+            fee: Fees.NONE
         });
+
         swaps[_swapID] = swap;
         swapStates[_swapID] = States.OPEN;
 
@@ -95,12 +140,19 @@ contract AtomicCloak {
     }
 
     function openERC20(
-        bytes32 _swapID,
+        bytes32 _qx,
+        bytes32 _qy,
         address payable _recipient,
         uint256 _timelock,
         address _tokenAddress,
         uint256 _value
     ) public payable {
+        address _swapID = address(
+            uint160(
+                uint256(keccak256(abi.encodePacked(_qx, _qy))) &
+                    0x00FFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF
+            )
+        );
         // The swapID is used also as commitment
         require(
             swapStates[_swapID] == States.INVALID,
@@ -137,7 +189,10 @@ contract AtomicCloak {
             value: msg.value,
             sender: payable(msg.sender),
             recipient: _recipient,
-            secretKey: new bytes(0)
+            secretKey: bytes32(0),
+            qx: _qx,
+            qy: _qy,
+            fee: Fees.NONE
         });
         swaps[_swapID] = swap;
         swapStates[_swapID] = States.OPEN;
@@ -146,13 +201,14 @@ contract AtomicCloak {
     }
 
     function close(
-        bytes32 _swapID,
-        bytes memory _secretKey
+        address _swapID,
+        bytes32 _secretKey
     ) public onlyOpenSwaps(_swapID) onlyWithSecretKey(_swapID, _secretKey) {
         Swap memory swap = swaps[_swapID];
         swaps[_swapID].secretKey = _secretKey;
         swapStates[_swapID] = States.CLOSED;
 
+        // TODO: implement fees to incentivize closing contracts as fast as possible.
         if (swap.tokenContract == ETH_TOKEN_CONTRACT) {
             // Transfer the ETH funds from this contract to the recipient.
             swap.recipient.transfer(swap.value);
@@ -166,7 +222,7 @@ contract AtomicCloak {
     }
 
     function expire(
-        bytes32 _swapID
+        address _swapID
     ) public onlyOpenSwaps(_swapID) onlyExpirableSwaps(_swapID) {
         Swap memory swap = swaps[_swapID];
         swapStates[_swapID] = States.EXPIRED;
@@ -184,8 +240,8 @@ contract AtomicCloak {
     }
 
     function getSecretKey(
-        bytes32 _swapID
-    ) public view onlyClosedSwaps(_swapID) returns (bytes memory secretKey) {
+        address _swapID
+    ) public view onlyClosedSwaps(_swapID) returns (bytes32 secretKey) {
         Swap memory swap = swaps[_swapID];
         return swap.secretKey;
     }
