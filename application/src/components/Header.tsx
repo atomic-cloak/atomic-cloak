af4dcd7d5ffa153659c4fbaa84e72cd222091ea5
Tommy <totaylor19@gmail.com>
2023-06-10 21:25:20 +0200

    turned cards as dynamic sections

--- application/src/components/Header.tsx
+++ application/src/components/Header.tsx
@@ -1,5 +1,7 @@
 import React from "react";
+import Link from "next/link";
 import Image from "next/image";
+import Container from "@/components/Container";
 import { ConnectButton } from "@rainbow-me/rainbowkit";
 
 const style = {
@@ -15,113 +17,125 @@ const style = {
 
 const Header: React.FC = () => {
   return (
-    <div className={style.wrapper}>
-      <div className={style.headerLogo}>
-        <Image src="/images/logo.png" alt="react" height={40} width={40} />
-      </div>
-      <div className={style.buttonsContainer}>
-        <ConnectButton.Custom>
-          {({
-            chain,
-            account,
-            mounted,
-            openChainModal,
-            openAccountModal,
-            openConnectModal,
-          }) => {
-            const ready = mounted;
-            const connected = ready && account && chain;
-            return (
-              <div
-                {...(!ready && {
-                  "aria-hidden": true,
-                  style: {
-                    opacity: 0,
-                    pointerEvents: "none",
-                    userSelect: "none",
-                  },
-                })}
-              >
-                {(() => {
-                  if (!connected) {
-                    return (
-                      <div
-                        onClick={() => openConnectModal()}
-                        className={`${style.button} ${style.buttonPadding}`}
-                      >
-                        <div
-                          className={`${style.buttonAccent} ${style.buttonPadding}`}
-                        >
-                          Connect Wallet
-                        </div>
-                      </div>
-                    );
-                  }
-
-                  if (chain.unsupported) {
-                    return (
-                      <button
-                        onClick={openChainModal}
-                        type="button"
-                        className={`${style.buttonAccent} ${style.buttonPadding}`}
-                      >
-                        <div className={`${style.buttonTextContainer}`}>
-                          Wrong Network
-                        </div>
-                      </button>
-                    );
-                  }
-
-                  return (
-                    <div style={{ display: "flex", gap: 12 }}>
-                      <button
-                        onClick={openChainModal}
-                        type="button"
-                        className={`${style.buttonAccent} ${style.buttonPadding}`}
-                      >
-                        {chain.hasIcon && (
+    <header className="py-10">
+      <Container>
+        <nav className="relative z-50 flex justify-between">
+          <div className="flex items-center md:gap-x-12">
+            <Link href="#" aria-label="Home">
+              <Image
+                src="/images/logo.png"
+                alt="react"
+                height={40}
+                width={40}
+              />
+            </Link>
+          </div>
+          <div className="flex items-center gap-x-5 md:gap-x-8">
+            <ConnectButton.Custom>
+              {({
+                chain,
+                account,
+                mounted,
+                openChainModal,
+                openAccountModal,
+                openConnectModal,
+              }) => {
+                const ready = mounted;
+                const connected = ready && account && chain;
+                return (
+                  <div
+                    {...(!ready && {
+                      "aria-hidden": true,
+                      style: {
+                        opacity: 0,
+                        pointerEvents: "none",
+                        userSelect: "none",
+                      },
+                    })}
+                  >
+                    {(() => {
+                      if (!connected) {
+                        return (
                           <div
-                            style={{
-                              background: chain.iconBackground,
-                              width: 20,
-                              height: 20,
-                              borderRadius: 999,
-                              overflow: "hidden",
-                              marginRight: 10,
-                            }}
+                            onClick={() => openConnectModal()}
+                            className={`${style.button} ${style.buttonPadding}`}
                           >
-                            {chain.iconUrl && (
-                              <img
-                                alt={chain.name ?? "Chain icon"}
-                                src={chain.iconUrl}
-                                style={{ width: 20, height: 20 }}
-                              />
-                            )}
+                            <div
+                              className={`${style.buttonAccent} ${style.buttonPadding}`}
+                            >
+                              Connect Wallet
+                            </div>
                           </div>
-                        )}
-                        <div className={`${style.buttonTextContainer}`}>
-                          {chain.name}
-                        </div>
-                      </button>
+                        );
+                      }
 
-                      <button
-                        onClick={openAccountModal}
-                        type="button"
-                        className={`${style.buttonAccent} ${style.buttonPadding}`}
-                      >
-                        <div className={`${style.buttonTextContainer}`}>
-                          {account.displayName}
+                      if (chain.unsupported) {
+                        return (
+                          <button
+                            onClick={openChainModal}
+                            type="button"
+                            className={`${style.buttonAccent} ${style.buttonPadding}`}
+                          >
+                            <div className={`${style.buttonTextContainer}`}>
+                              Wrong Network
+                            </div>
+                          </button>
+                        );
+                      }
+
+                      return (
+                        <div style={{ display: "flex", gap: 12 }}>
+                          <button
+                            onClick={openChainModal}
+                            type="button"
+                            className={`${style.buttonAccent} ${style.buttonPadding}`}
+                          >
+                            {chain.hasIcon && (
+                              <div
+                                style={{
+                                  background: chain.iconBackground,
+                                  width: 20,
+                                  height: 20,
+                                  borderRadius: 999,
+                                  overflow: "hidden",
+                                  marginRight: 10,
+                                }}
+                              >
+                                {chain.iconUrl && (
+                                  <img
+                                    alt={chain.name ?? "Chain icon"}
+                                    src={chain.iconUrl}
+                                    style={{ width: 20, height: 20 }}
+                                  />
+                                )}
+                              </div>
+                            )}
+                            <div className={`${style.buttonTextContainer}`}>
+                              {chain.name}
+                            </div>
+                          </button>
+
+                          <button
+                            onClick={openAccountModal}
+                            type="button"
+                            className={`${style.buttonAccent} ${style.buttonPadding}`}
+                          >
+                            <div className={`${style.buttonTextContainer}`}>
+                              {account.displayName}
+                            </div>
+                          </button>
                         </div>
-                      </button>
-                    </div>
-                  );
-                })()}
-              </div>
-            );
-          }}
-        </ConnectButton.Custom>
-      </div>
-    </div>
+                      );
+                    })()}
+                  </div>
+                );
+              }}
+            </ConnectButton.Custom>
+            <div className="-mr-1 md:hidden">{/* <MobileNavigation /> */}</div>
+          </div>
+        </nav>
+      </Container>
+    </header>
   );
 };
 export default Header;
