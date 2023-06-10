27e82e1db1610ffa15e130817cf12858543ddd3c
Aidas <aidas.jankauskas.dev@gmail.com>
2023-06-10 16:59:39 +0000

    Updated graphl endpoints

--- backend/src/constants.ts
+++ backend/src/constants.ts
@@ -1,8 +1,8 @@
 import abi from './data/abi/AtomicCloak.json'
 
 export const graphqlEndpoints = {
-  Sepolia: 'https://api.studio.thegraph.com/proxy/48185/atomiccloak/v0.0.6',
-  Mumbai: 'https://api.studio.thegraph.com/proxy/48185/atomiccloak/v0.0.6'
+  Sepolia: 'https://api.studio.thegraph.com/proxy/48185/atomic-cloak-sepolia/version/latest',
+  Mumbai: 'https://api.studio.thegraph.com/query/48185/atomic-cloack-mumbai/version/latest'
 }
 
 export const atomicCloakABI = abi.abi
