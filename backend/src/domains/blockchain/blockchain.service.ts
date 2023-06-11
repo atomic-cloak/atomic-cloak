c30aad316d1d5fedcff74285a9cf534f5dfae325
Alessandro Ricottone <ricott2@gmail.com>
2023-06-11 10:26:05 +0200

    add chains to backend

--- backend/src/domains/blockchain/blockchain.service.ts
+++ backend/src/domains/blockchain/blockchain.service.ts
@@ -5,7 +5,10 @@ export const getAtomicCloakContract = async (chainId) => {
   const contractAddresses = {
     11155111: process.env.ATOMIC_CLOAK_ADDRESS_SEPOLIA,
     80001: process.env.ATOMIC_CLOAK_ADDRESS_MUMBAI,
-    420: process.env.ATOMIC_CLOAK_ADDRESS_OPTIMISM_GOERLI
+    420: process.env.ATOMIC_CLOAK_ADDRESS_OPTIMISM_GOERLI,
+    5001: process.env.ATOMIC_CLOAK_ADDRESS_MANTLE,
+    280: process.env.ATOMIC_CLOAK_ADDRESS_ZKSYNC,
+    167005: process.env.ATOMIC_CLOAK_ADDRESS_TAIKO
   }
 
   const provider = new ethers.providers.JsonRpcProvider(rpcProviders[chainId])
